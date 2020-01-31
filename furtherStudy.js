ff61195c538a8ad127dbc81a4109faaca648db5c
Your Name <you@example.com>
2020-01-31 12:45:37 -0800

    added working lines to wordsInCommon function

--- furtherStudy.js
+++ furtherStudy.js
@@ -3,6 +3,19 @@
 
 function wordsInCommon(words1, words2) {
   // Replace this with your code
+
+  const words1_set = new Set(words1);
+  const words2_set = new Set(words2)
+
+  const result = new Set();
+
+  for (const word of words1_set) {
+    if (words2_set.has(word)) {
+      result.add(word);
+    }
+  }
+
+  return Array.from(result);
 }
 
 
